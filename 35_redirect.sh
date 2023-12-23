265fe1b1249bffbb5b539c50755a75f2c0633773
Neeraj <neerajkumarghildiyal@gmail.com>
2023-12-24 00:38:13 +0530

    Added redirects in scripts

---
+++ 35_redirect.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# suppose hume kisi command ke output ko ek file me save karna hai then we use redirect >, >> double arrow means append karna same file me
+
+# ping cmd ke output ko aap kisi file me bhi store kar sakte ho 
+
+ping -c 1 www.google.com > redirect.log
+
+
